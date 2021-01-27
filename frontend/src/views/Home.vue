99c28e0d9386566e9a857b746c6c41c027f89288
成雨 <rensailong@zhongan.io>
2021-01-27 22:04:27 +0800

    update

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,6 +6,53 @@
       src="../assets/appicon.png"
       :style="{ height: '400px' }"
     />
+    <div
+      class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
+    >
+      <div class="flex-shrink-0">
+        <img
+          class="h-12 w-12"
+          src="../assets/appicon.png"
+          alt="ChitChat Logo"
+        />
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
+    <div
+      class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6"
+    >
+      <img
+        class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0"
+        src="../assets/appicon.png"
+        alt="Woman's Face"
+      />
+      <div class="text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg text-black font-semibold">
+            Erin Lindford
+          </p>
+          <p class="text-gray-500 font-medium">
+            Product Engineer
+          </p>
+        </div>
+        <button
+          class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+        >
+          Message
+        </button>
+      </div>
+    </div>
+    <button
+      class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+    >
+      Click Me
+    </button>
+    <button class="btn btn-green">
+      Button
+    </button>
     <HelloWorld :msg="message" />
   </div>
 </template>
@@ -32,3 +79,13 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped lang="stylus">
+.btn
+  @apply py-2 px-4 font-semibold rounded-lg shadow-md
+
+.btn-green
+  @apply text-white bg-green-500
+  &:hover
+    @apply bg-green-700
+</style>
